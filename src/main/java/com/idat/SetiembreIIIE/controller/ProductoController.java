641648bed5a4d46d2d8327ebe91bf13d059a7bce
JOHALDO <johaldo_oliver@hotmail.com>
2022-10-02 18:14:44 -0500

    api con orm

--- src/main/java/com/idat/SetiembreIIIE/controller/ProductoController.java
+++ src/main/java/com/idat/SetiembreIIIE/controller/ProductoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.idat.SetiembreIIIE.model.Producto;
-import com.idat.SetiembreIIIE.repository.ProductoRepository;
 import com.idat.SetiembreIIIE.service.ProductoService;
 
 @RestController
